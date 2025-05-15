f154710534e727c2223de7f3edd462b3dc16bd85
Liang N7 <55042080+Enchi-Liang@users.noreply.github.com>
2025-05-16 02:50:06 +0800

    test1

--- decry2fa.py
+++ decry2fa.py
@@ -3,10 +3,25 @@ import os
 import pyotp
 
 
+def totp():
+    '''
+    check if the key is valid
+    '''
+    totp = pyotp.TOTP(key)
+    return totp.verify(key)
+
+
 '''
 ask the key from user (the 6 key from 2 fa )
 '''
 
+flag_2fa = False
+while flag_2fa == False:
+    if len(key) == 6 and key.isdigit() and totp(flag_2fa):
+        flag_2fa = True
+    else:
+        print("Please enter a valid 6 digit key")
+        key = input("Key: ")
 
 
 
